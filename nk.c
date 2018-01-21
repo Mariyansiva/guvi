cfab60c3fed9680cdec79168ee07c55aaa86127d
Mariyansiva <30828524+Mariyansiva@users.noreply.github.com>
2018-01-21 09:17:40 +0530

    Create nk.c

---
+++ nk.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main() 
+{
+int i,j,N,K ,a=0;
+scanf("%d",&N);
+scanf("%d",&K);
+printf("Input\n %d %d\n",N,K);
+for(i=1;i<=N;i++)
+{
+printf("%d",i);
+}
+for(j=1;j<=K;j++)
+{
+a +=j;
+}
+printf("\nOutput\n %d \n",a;
+return 0;
+}

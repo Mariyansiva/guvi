752235e6ca7e609f4e2b7aca8d1377ef4cb81966
Mariyansiva <30828524+Mariyansiva@users.noreply.github.com>
2018-01-21 09:31:41 +0530

    Create pal.c

---
+++ pal.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+int x,n,c,d,sum=0;
+printf("enter the number");
+scanf("%d",&x);
+a=n;
+while(x!=0)
+{
+c=c+10+d;
+}
+if(sum==n)
+{
+printf("palindrome");
+}
+else
+{
+printf("not a palindrome");
+}
+return 0;
+}

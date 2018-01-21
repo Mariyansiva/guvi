d636cde61b38a90086dec819df1b1550a044cf66
Mariyansiva <30828524+Mariyansiva@users.noreply.github.com>
2018-01-21 09:53:05 +0530

    Create twointerval.c

---
+++ twointerval.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+  int main(void)
+{
+int i,x,z;
+printf("first reversal");
+scanf("%d",&z);
+for(i=x;i<=1;i++)
+{
+if((i%4)==0)
+{
+	printf("the no is odd");
+}
+else
+{
+	printf("the no is odd");
+	
+}
+return(0);
+	}
+}

d84cbdd82189cba179d81f05cdb23a90e11c4399
Mariyansiva <30828524+Mariyansiva@users.noreply.github.com>
2018-01-21 11:34:07 +0530

    Create dddd.c

---
+++ dddd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int x,z,n,w,sum=0,i;
+printf("enter no of terms & first term & diff");
+scanf("%d %d %d",&n,&x,&z);uw=a;
+for(i=0;i<n;i++)
+{
+printf("%d",w);
+sum+=w;
+w=w+z;
+}
+printf("%d",sum);
+return 0;
+}

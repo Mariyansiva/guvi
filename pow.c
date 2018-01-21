d613bda83ee55f27f59b346843d9c24e3daa9487
Mariyansiva <30828524+Mariyansiva@users.noreply.github.com>
2018-01-21 09:24:19 +0530

    Create pow.c

---
+++ pow.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main() 
+{
+int i,j,N,K,x=0;
+scanf("%d",&N);
+scanf("%d",&K);
+printf("Input\n %d %d\n",N,K);
+for(i=1;i<=N;i++)
+{
+printf("%d",i);
+}
+for(j=1;j<=K;j++)
+{
+x +=j;
+}
+printf("\nOutput\n %d \n",b);
+return 0;
+}

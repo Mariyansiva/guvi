81fe6b6a4e6d69672d0fbeb4aa324ad6070dcc16
Mariyansiva <30828524+Mariyansiva@users.noreply.github.com>
2018-01-25 16:38:35 +0530

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int x[50],i,s;
+printf("enter a number");
+scanf("%d",&s);
+for(i=o;i<s;i++);
+{
+scanf("%d",&x[i]);
+}
+for(i=0;i<s;i++)
+{
+printf("%d\t %d",x[i],i);
+}
+return 0;
+}

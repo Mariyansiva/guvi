93db213770eab2ee97c1ffa508a22872387105a4
Mariyansiva <30828524+Mariyansiva@users.noreply.github.com>
2018-02-01 16:51:15 +0530

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,12 @@
+
+
+#include<stdio.h>
+void main()
+{
+        int a,b,c,d,x,y;
+        printf("hr  min :");
+        scanf("%d %d %d %d",&a,&b,&c,&d);
+  x=(a-b);
+  y=(c-d);
+printf("hour &mins%d %d:",x,y);
+}

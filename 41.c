e8f8d2c72fb9641d2558fe51a4dcb4baa07d903a
Mariyansiva <30828524+Mariyansiva@users.noreply.github.com>
2018-02-09 09:12:54 +0000

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+	char s[50];
+	int i,n;
+		gets(s);
+	printf("value of n");
+	scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+    printf("\n %s",s);
+}
+}

6397aa8f73bc0d1fbdc30ff787c372fa6662c326
Mariyansiva <30828524+Mariyansiva@users.noreply.github.com>
2018-02-27 16:27:42 +0530

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,17 @@
+
+
+#include <stdio.h>
+ 
+int main(void) 
+{
+int i,j,sum;
+printf("enter the value of i:");
+scanf("%d",&i);
+printf("enter the value of j:");
+scanf("%d",&j);
+sum=i+j;
+printf("\nsum of value:%d",sum);	 
+	        return 0;
+}
+ 
+

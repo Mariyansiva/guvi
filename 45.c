83bf8b61cca2512ed25609acc5ce8361ff673d2e
Mariyansiva <30828524+Mariyansiva@users.noreply.github.com>
2018-03-01 16:32:54 +0530

    Update 45.c

--- 45.c
+++ 45.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 void main()
-{  long long z;
+{  long long x;
    int count=0;
    printf("Enter the number: ");
-   scanf("%d",&z);
-   while(z!=0)
+   scanf("%d",&x);
+   while(x!=0)
   { 
-  z=z/10;
+  x=x/10;
   ++count;
   }
     printf("The count of digits in a no:%d",count);

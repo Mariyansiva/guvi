cbdc0c229d84d67e4e39634df2fc603f385df297
Mariyansiva <30828524+Mariyansiva@users.noreply.github.com>
2018-03-01 16:09:35 +0530

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+
+    char x[10],y[10];
+    printf("1st value: ");
+scanf("%s",&x);
+printf("\n2nd value: ");
+scanf("%s",&y);
+
+ printf("\n%s%s",x,y);
+ return 0;
+}
+
+
+

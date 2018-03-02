d3b4723597dae03ac67b2e6db0050a9e748ffe70
sivasethupathi.r <30828524+Mariyansiva@users.noreply.github.com>
2018-03-02 16:23:25 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main()
+{
+int b,y,z,s;
+printf("enter the value");
+scanf("%d",&b);
+printf("enter the value");
+scanf("%d",&s);
+z=(b*s);
+if(z%2==0)
+    printf("even");
+else
+printf("\nodd");
+getch();
+}

fe5a6c4b37ed1febced1911e5dcc2480bea54256
sivasethupathi.r <30828524+Mariyansiva@users.noreply.github.com>
2018-03-02 16:15:20 +0530

    Create 54.c

---
+++ 54.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main()
+{
+int b,y,z;
+printf("enter the value");
+scanf("%d",&b);
+ z=(b-1);
+ y=(b%10);
+if(y==0)
+printf("%d",b);
+else(y!=0);
+printf("%d",z);
+getch();
+}

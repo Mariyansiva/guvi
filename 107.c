4ddc39def71c88387a9894e318570d019af00f35
sivasethupathi.r <30828524+Mariyansiva@users.noreply.github.com>
2018-03-09 16:25:34 +0530

    Create 107.c

---
+++ 107.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z,sum;
+    printf("1st num:");
+    scanf("%d",&x);
+    printf("2nd num:");
+    scanf("%d",&y);
+    printf("3rd num:");
+    scanf("%d",&z);
+sum=(x*y/z);
+printf("%d",sum);
+    
+}

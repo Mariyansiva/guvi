a5801ee3ee2fe0565c65b1c1b27e0f5337a9c964
sivasethupathi.r <30828524+Mariyansiva@users.noreply.github.com>
2018-03-16 15:42:43 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a,x;
+    int add;
+    printf("the no is:");
+    scanf("%d",&a);
+x=a%7;
+if(x==0)
+printf("yes");
+else
+printf("no");
+
+    return 0;
+}

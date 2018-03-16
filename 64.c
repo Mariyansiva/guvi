b52330d793fdb86b80c4eb973f3463a6297c6e7b
sivasethupathi.r <30828524+Mariyansiva@users.noreply.github.com>
2018-03-16 15:48:51 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a,x,b;
+    int add;
+    printf("1st no:");
+    scanf("%d",&a);
+        printf("2nd no:");
+scanf("%d",&b);
+add=a+b;
+x=add%2;
+if(x==0)
+printf("even");
+else
+printf("odd");
+
+    return 0;
+}
+
